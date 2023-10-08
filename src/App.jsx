c05f037da2e5d9ecf071fc05e030090fd8fa96b0
Shirley Zhan <shirleyzhan3@gmail.com>
2023-10-07 21:15:51 -0400

    Adding basic format of website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar />
+      <Home/>
+      <Route />
     </BrowserRouter>
   );
 };
